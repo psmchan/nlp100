47bf00813345103b0b6590b2de5e02ea595e3a2e
psmchan <63440805+psmchan@users.noreply.github.com>
2020-05-07 17:35:06 +0900

    n03:円周率

---
+++ n03.py
@@ -0,0 +1,9 @@
+Piwords = []
+Pi = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+words = Pi.split() # 単語ごとに取り出す
+kosu = len(words)
+while kosu > 0 : # 単語の個数が0になるまで
+    words[0]
+    Piwords = [word[0] for word in words]
+    kosu -= 1
+print(Piwords)
