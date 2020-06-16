ee4d01075215fdd5c00d15224fa2fcd24ca36fe7
psmchan <63440805+psmchan@users.noreply.github.com>
2020-06-16 17:21:50 +0900

    n10

---
+++ n10.py
@@ -0,0 +1,16 @@
+"""
+行数をカウントせよ．確認にはwcコマンドを用いよ．
+"""
+"""
+wcコマンド
+「wc」　テキストファイルの行数や単語数、文字列を数えるコマンド
+-l # 改行の数を表示
+"""
+import sys
+
+count = 0
+
+with open("popular-names.txt") as f : # popular-names.txtをfとして開く
+    for line in f : # 行数をカウントする
+        count += 1 # カウントしていく
+print(count)
