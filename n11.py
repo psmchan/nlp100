90ba456a80b4f85ae70e8666848fcc4168d9d39a
psmchan <63440805+psmchan@users.noreply.github.com>
2020-06-17 18:30:58 +0900

    n11:改良版

--- n11.py
+++ n11.py
@@ -1,11 +1,19 @@
 """
 タブ1文字につきスペース1文字に置換せよ．
 確認にはsedコマンド，trコマンド，もしくはexpandコマンドを用いよ．
+
+sed s/置換対象文字列/置換後文字列/ファイル名
+
+"""
+"""
+sys.argv[0] = n11.py
+sys.argv[1] = popular-names.txt
+sys.argv[2] = answer_11.txt
 """
 import sys
 
-with open("popular-names.txt") as f : # popular-names.txtをfとして開く
-    for tab in f :
-        tab = tab.strip() # 出力したときに1行空いてたから改行抜いた
+with open(sys.argv[1],"r") as f1 , open(sys.argv[2],"w") as f2 :
+    for tab in f1 :
+        #tab = tab.strip() # ファイルに書き込んだら改行が元から無かったから消した
         tab = tab.replace("\t"," ")# タブをスペース1文字に置換
-        print(tab)
+        f2.write(tab)
