767c248e6f1ae74fb1a3a8cac68ef63075650c41
psmchan <63440805+psmchan@users.noreply.github.com>
2020-06-26 17:37:47 +0900

    n18:pandas使った

--- n18.py
+++ n18.py
@@ -1,31 +1,28 @@
 """
 各行を3コラム目の数値の逆順で整列せよ（注意: 各行の内容は変更せずに並び替えよ）．
 確認にはsortコマンドを用いよ（この問題はコマンドで実行した時の結果と合わなくてもよい）．
+
+pandas
+import pandas as pd
+import numpy as np
+
+pandasのテキストファイル読み込み関数 = read_table()
+変数 = pd.read_table("ファイル名")
+ヘッダーがない場合は、("ファイル名",header = None)
+テキストファイルとして出力
+df.to_csv("ファイル名")
+ヘッダーを消す場合は  header = None
+行番号を消す場合は　  index = None
+区切り文字を指定する場合は   sep = ""(デフォルトは",")
+値の小さい順にソート
+変数.sort_values(ソートする場所)
+値の大きい順にソート
+変数.sort_values(ソートする場所,ascending = False)
 """
 
-import sys
+import pandas as pd
 
-emp = ""
-n = 0
-
-with open(sys.argv[1],"r") as f1, open(sys.argv[2],"w") as f2 :
-    for word in f1 :
-        col3 = word.split("\t")[2]
-        if len(col3) == 4 :
-            col3_rjust = col3.rjust(5)
-        else :
-            col3_rjust = col3
-        emp += col3_rjust + "\n"
-    else :
-        col3list = emp.split("\n")
-        sortlist = sorted(col3list,reverse = True)
-        #for num in sortlist :
-        #    f2.write(num + "\n")
-
-with open(sys.argv[1],"r") as f1, open(sys.argv[2],"w") as f2 :
-    for line in f1 :
-        if sortlist[n] in line :
-            f2.write(line)
-            n += 1
-        else :
-            continue
+# データフレーム df を作成
+dates = pd.read_table("popular-names.txt",header = None)
+ans = dates.sort_values(2, ascending = False)
+ans.to_csv("answer_18.txt",header = None, index = None, sep = "\t")
