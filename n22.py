9b8ef7eb41e1419bf5cb4793af756625cc155eaf
psmchan <63440805+psmchan@users.noreply.github.com>
2020-07-08 16:56:38 +0900

    n22:多分これで大丈夫

--- n22.py
+++ n22.py
@@ -1,3 +1,7 @@
+"""
+re.sub : マッチする部分を置換
+〇〇 = re.sub(正規表現パターン,置換先文字列,処理対象の文字列)
+"""
 import pandas as pd
 import json
 import re
@@ -6,9 +10,11 @@ df_gz = pd.read_json("jawiki-country.json.gz",compression = "infer", lines = Tru
 uk = df_gz.query('title == "イギリス"')["text"].values[0]
 #print(ans)
 word = "\[\[Category:(.*)\]\]" # 抽出したい部分を()でくくる,正規表現パターンをグルーピング
+remove_word = "\|.*"
 for line in uk.split("\n") :
     ans = re.search(word,line)
     if ans != None :
-        print(ans.group(1))
+        answer = re.sub(remove_word,"",ans.group(1))
+        print(answer)
     else :
         continue
