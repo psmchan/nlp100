2287fec15b3356d7aa914bd8a2e8a666c36a8d00
psmchan <63440805+psmchan@users.noreply.github.com>
2020-07-30 13:31:38 +0900

    n26

---
+++ n26.py
@@ -0,0 +1,26 @@
+"""
+25の処理時に，テンプレートの値からMediaWikiの強調マークアップ
+（弱い強調，強調，強い強調のすべて）を除去してテキストに変換せよ
+"""
+import re
+import pprint
+import n25
+
+def em() :
+    tem1 = n25.tem()
+
+    empha = r"\'{2,5}"
+
+    for key in tem1 :
+        value = tem1[key]
+        em_s = re.search(empha,value)
+        if em_s != None :
+            value = re.sub(empha,"",value)
+            tem1[key] = value
+        else :
+            continue
+    return tem1
+
+if __name__ == "__main__" :
+    ans = em()
+    pprint.pprint(ans,width = 400)
