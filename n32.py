2226618a5dae198cd596cb056a74e0f111f22c29
psmchan <63440805+psmchan@users.noreply.github.com>
2020-08-28 18:40:30 +0900

    n32

---
+++ n32.py
@@ -0,0 +1,12 @@
+"""
+動詞の原形をすべて抽出せよ．
+"""
+
+import n30
+
+ver = n30.neko()
+
+for neko_list in ver :
+    for neko_dict in neko_list :
+        if neko_dict["pos"] == "動詞" :
+            print(neko_dict["base"])
